e50697bb1d31423878c6e8f49dc557e3fe812949
PranaviJamdar05 <pranavijamdar41@gmail.com>
2026-09-06 10:40:37 +0530

    Document function types and parameters in functionparameter.py
    Add a table summarizing function types and parameters.

---
+++ functionparameter.py
@@ -0,0 +1,8 @@
+#| Code                                       | Type                  |
+#| ------------------------------------------ | --------------------- |
+#| `is_even()`                                | Normal function       |
+#| `is_odd()`                                 | Normal function       |
+#| `check_num()`                              | Higher-Order Function |
+#| `condition`                                | Function parameter    |
+#| `is_even` in `check_num(numbers, is_even)` | Callback              |
+#| `is_odd` in `check_num(numbers, is_odd)`   | Callback              |
